da76b4fc7393cf3c1d0c16d5a3dc07128a0bd278
Sarju88 <1093459@lwsd.org>
2026-03-29 20:31:58 -0700

    Fix web food visibility by preloading pickup textures

--- food/food_pickup.gd
+++ food/food_pickup.gd
@@ -2,19 +2,19 @@ extends Area2D
 
 @export var heal_amount: float = 20.0
 
-const FOOD_TEXTURE_PATHS := [
-	"res://food/tile_0000.png",
-	"res://food/tile_0005.png",
-	"res://food/tile_0010.png",
-	"res://food/tile_0015.png",
-	"res://food/tile_0020.png",
-	"res://food/tile_0025.png",
-	"res://food/tile_0030.png",
-	"res://food/tile_0035.png",
-	"res://food/tile_0040.png",
-	"res://food/tile_0045.png",
-	"res://food/tile_0050.png",
-	"res://food/tile_0055.png"
+const FOOD_TEXTURES: Array[Texture2D] = [
+	preload("res://food/tile_0000.png"),
+	preload("res://food/tile_0005.png"),
+	preload("res://food/tile_0010.png"),
+	preload("res://food/tile_0015.png"),
+	preload("res://food/tile_0020.png"),
+	preload("res://food/tile_0025.png"),
+	preload("res://food/tile_0030.png"),
+	preload("res://food/tile_0035.png"),
+	preload("res://food/tile_0040.png"),
+	preload("res://food/tile_0045.png"),
+	preload("res://food/tile_0050.png"),
+	preload("res://food/tile_0055.png")
 ]
 
 @onready var sprite: Sprite2D = $Sprite2D
@@ -22,8 +22,9 @@ const FOOD_TEXTURE_PATHS := [
 
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
-	sprite.texture = _load_texture_from_file(FOOD_TEXTURE_PATHS.pick_random())
+	sprite.texture = FOOD_TEXTURES.pick_random()
 	sprite.scale = Vector2.ONE * randf_range(1.6, 2.1)
+	sprite.z_index = 5
 
 
 func _on_body_entered(body: Node) -> void:
@@ -32,11 +33,3 @@ func _on_body_entered(body: Node) -> void:
 	if body.has_method("heal"):
 		body.heal(heal_amount)
 	queue_free()
-
-
-func _load_texture_from_file(path: String) -> Texture2D:
-	var image := Image.new()
-	var err := image.load(path)
-	if err != OK:
-		return null
-	return ImageTexture.create_from_image(image)
